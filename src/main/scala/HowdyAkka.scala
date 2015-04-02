550cadd7c7781b0a317b0e3bc8c95e78fcc22ad1
nrkin <nachiketkakatkar@gmail.com>
2015-04-02 14:48:24 +0530

    Create multiple actors and their interaction

--- src/main/scala/HowdyAkka.scala
+++ src/main/scala/HowdyAkka.scala
@@ -1,37 +1,47 @@
 import akka.actor.{ActorSystem, ActorLogging, Actor, Props}
 
-case object Ticket
-case object FullPint
-case object EmptyPint
+case class Ticket(quantity: Int)
+case class FullPint(number: Int)
+case class EmptyPint(number: Int)
 
 class BarTender extends Actor with ActorLogging {
+  var total = 0
   def receive = {
-    case Ticket =>
-      log.info("1 pint coming right up")
+    case Ticket(quantity) =>
+      total = total + quantity
 
-      Thread.sleep(1000)
+      log.info(s"I'll get $quantity pints for [${sender.path}]")
 
-      log.info("Your pint is ready, here you go")
+      for (number <- 1 to quantity) {
+        log.info(s"Pint $number is coming right up for [${sender.path}]")
+        Thread.sleep(1000)
+        log.info(s"Pint $number is ready, here you go [${sender.path}]")
+        sender ! FullPint(number)
+      }
 
-      sender ! FullPint
+    case EmptyPint(number) =>
+      total match {
+        case 1 =>
+          log.info("Ya'll drank those pints quick, time to close up shop")
+          context.system.shutdown()
 
-    case EmptyPint =>
-      log.info("I think you're done for the day")
-
-      context.system.shutdown()
+        case n =>
+          total = total - 1
+          log.info(s"You drank pint $number quick, but there are still $total pints left")
+      }
   }
 }
 
 class Person extends Actor with ActorLogging {
   def receive = {
-    case FullPint =>
-      log.info("I'll make short work of this")
+    case FullPint(number) =>
+      log.info(s"I'll make short work of this pint $number")
 
       Thread.sleep(1000)
 
-      log.info("I'm ready for the next")
+      log.info(s"Done, here is the empty glass for pint $number")
 
-      sender ! EmptyPint
+      sender ! EmptyPint(number)
   }
 }
 
@@ -40,9 +50,12 @@ object HowdyAkka extends App {
 
   val zed = system.actorOf(Props(new BarTender), "zed")
 
-  val alice = system.actorOf(Props(new Person), "alice")
+  val alice   = system.actorOf(Props(new Person), "alice")
+  val bob     = system.actorOf(Props(new Person), "bob")
+  val charlie = system.actorOf(Props(new Person), "charlie")
 
-  zed.tell(Ticket, alice)
+  zed.tell(Ticket(2), alice)
+  zed.tell(Ticket(3), bob)
+  zed.tell(Ticket(1), charlie)
 
-  system.awaitTermination()
 }
